6831a6a237f350d34b115d63c173edf711b5c72a
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-08-27 16:00:25 +0530

    Added comments

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+// Selecting elements
 const todoForm = document.querySelector("#Todo-form");
 const todoInput = document.querySelector("#Todo-input");
 const todosUl = document.querySelector("#Todos");
 
+// Getting todos from local storage
 const todos = JSON.parse(localStorage.getItem("todos"));
 
+// Adding the todos from local storage
 if (todos) {
   todos.forEach((todo) => addTodo(todo));
 }
 
+// On submit add todo along with the event listeners to each todo
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addTodo();
@@ -16,18 +20,21 @@ todoForm.addEventListener("submit", (e) => {
 function addTodo(todo) {
   let todoText = todoInput.value;
 
+  // If todo passed as arg
   if (todo) {
     todoText = todo.text;
   }
 
   if (todoText) {
     const todoEl = document.createElement("li");
+    // if todo from local storage is completed
     if (todo && todo.completed) {
       todoEl.classList.add("completed");
     }
 
     todoEl.innerText = todoText;
 
+    // Adding eventlistener to each todo list item
     todoEl.addEventListener("click", () => {
       saveTodos();
       todoEl.classList.toggle("completed");
@@ -49,6 +56,7 @@ function addTodo(todo) {
   }
 }
 
+// Save to local storage each time added, deleted or completed
 function saveTodos() {
   let todosEl = document.querySelectorAll("li");
 
