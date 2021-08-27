726449fb832026b33d56dd41b58cb89a8cbb13bd
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-08-27 12:37:38 +0530

    Basic functionality added

---
+++ app.js
@@ -0,0 +1,65 @@
+const todoForm = document.querySelector("#Todo-form");
+const todoInput = document.querySelector("#Todo-input");
+const todosUl = document.querySelector("#Todos");
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos) {
+  todos.forEach(todo => addTodo(todo));
+}
+
+todoForm.addEventListener('submit', e => {
+  e.preventDefault();
+  addTodo();
+})
+
+function addTodo(todo) {
+  let todoText = todoInput.value;
+
+  if(todo) {
+    todoText = todo.text;
+  }
+
+  if(todoText) {
+    const todoEl = document.createElement("li");
+    if(todo && todo.completed) {
+      todoEl.classList.add("completed")
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener('click', () => {
+      saveTodos();
+      todoEl.classList.toggle('completed')
+    });
+
+    todoEl.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+
+      saveTodos();
+    });
+    
+    todosUl.appendChild(todoEl);
+
+    todoInput.value = "";
+
+    saveTodos();
+  }
+}
+
+function saveTodos(){
+  let todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach(todoEl => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains('completed')
+    })
+  })
+
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
